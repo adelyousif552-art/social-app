d9fa6097e88a66deff8beaaba0fb263a5434d601
adelyousif552-art <adelyousif552@gmail.com>
2026-08-04 21:54:37 +0300

    22 commit

--- src/components/Postcard/Postcard.jsx
+++ src/components/Postcard/Postcard.jsx
@@ -348,7 +348,7 @@ headers:{
             </div>
            <span className="text-sm text-gray-600/80">{commentsCount} Comments</span>
         </div>
-        <div className="reactionbuttons *:cursor-pointer *:hover:bg-gray-100 *:transition-colors *:duration-200 flex *:grow *:p-3">
+        <div className="reactionbuttons *:cursor-pointer lg:*:hover:bg-gray-100 *:transition-colors *:duration-200 flex *:grow *:p-3">
             <button onClick={putlikes} className={`border-y ${like?'text-blue-600 font-semibold':''}  transition-colors duration-400  border-gray-500/70 `}>
                 <FontAwesomeIcon className={``} icon={faThumbsUp}/>
                 <span>Like</span>
